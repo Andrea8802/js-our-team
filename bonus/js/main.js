07379554549d4fdf62f2ba396bbddebb4cfb749d
Andrea8802 <andreabonaventura802@gmail.com>
2022-11-21 15:47:07 +0100

    bonus folder imported

--- bonus/js/main.js
+++ bonus/js/main.js
@@ -2,49 +2,47 @@
 
 // DOM
 const output = document.getElementById("output");
-
-// Object
 const membriTeam = [
     {
         "nome" : "Wayne Barnett",
         "ruolo" : "Founder & CEO",
-        "foto" : "wayne-barnett-founder-ceo.jpg"
+        "foto" : "img/wayne-barnett-founder-ceo.jpg"
     },
 
     {
         "nome" : "Angela Caroll",
         "ruolo" : "Chief Editor",
-        "foto" : "angela-caroll-chief-editor.jpg"
+        "foto" : "img/angela-caroll-chief-editor.jpg"
     },
 
     {
         "nome" : "Walter Gordon",
         "ruolo" : "Office Manager",
-        "foto" : "walter-gordon-office-manager.jpg"
+        "foto" : "img/walter-gordon-office-manager.jpg"
     },
 
     {
         "nome" : "Angela Lopez",
         "ruolo" : "Social Medi Manager",
-        "foto" : "angela-lopez-social-media-manager.jpg"
+        "foto" : "img/angela-lopez-social-media-manager.jpg"
     },
 
     {
         "nome" : "Scott Estrada",
         "ruolo" : "Developer",
-        "foto" : "scott-estrada-developer.jpg"
+        "foto" : "img/scott-estrada-developer.jpg"
     },
 
     {
         "nome" : "Barbara Ramos",
         "ruolo" : "Graphic Designer",
-        "foto" : "barbara-ramos-graphic-designer.jpg"
+        "foto" : "img/barbara-ramos-graphic-designer.jpg"
     }
 ];
 
 
 for (let i = 0; i < membriTeam.length; i++){
-    for (key in membriTeam[i]){
-        output.innerHTML += `<li>${key} ${membriTeam[i][key]}</li>`;
-    }
+    output.innerHTML += `<li>${membriTeam[i].nome}</li>`;
+    output.innerHTML += `<li>${membriTeam[i].ruolo}</li>`;
+    output.innerHTML += `<img src="${membriTeam[i].foto}">`
 }
